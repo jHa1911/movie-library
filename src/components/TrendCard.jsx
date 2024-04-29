63b31af54c50889e1b74bd1943a58ba9f40ba571
jHa1911 <sjha13390@gmail.com>
2024-04-29 23:59:46 +0530

    added trends card

---
+++ src/components/TrendCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+function TrendCard({ slide }) {
+    return (
+        <div className="trend-card">
+            <img className="img-fluid" src={slide.previewImg} alt="" />
+            <a href="#">
+                Add to calendar <ion-icon name="calendar-outline"></ion-icon>
+            </a>
+        </div>
+    );
+}
+export default TrendCard;
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./main.css";
 import Schedule from "./Schedule";
+import Trend from "./Trend";
 
 function Main() {
     return <main>
         <Schedule />
+        <Trend/>
     </main>;
 }
 
